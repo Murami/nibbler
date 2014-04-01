d8f75ff02cf7c7ff1e57861693b18082d508ed60
guerot_a <anthony.guerot@gmail.com>
2014-04-01 14:32:49 +0200

    .

--- core/src/Snake.cpp
+++ core/src/Snake.cpp
@@ -5,13 +5,18 @@
 // Login   <guerot_a@epitech.net>
 //
 // Started on  Thu Mar 27 14:43:21 2014 guerot_a
-// Last update Sun Mar 30 22:45:09 2014 guerot_a
+// Last update Sun Mar 30 22:57:08 2014 guerot_a
 //
 
 #include "Snake.hpp"
 
-Snake::Snake()
+Snake::Snake() :
+  m_direction(1, 0)
 {
+  m_snakeLimbs.push_back(Vector2i(0, 0));
+  m_snakeLimbs.push_back(Vector2i(1, 0));
+  m_snakeLimbs.push_back(Vector2i(2, 0));
+  m_snakeLimbs.push_back(Vector2i(3, 0));
 }
 
 Snake::~Snake()
@@ -20,18 +25,22 @@ Snake::~Snake()
 
 void	Snake::enableBoost()
 {
+  m_boost = true;
 }
 
 void	Snake::disableBoost()
 {
+  m_boost = false;
 }
 
 void	Snake::turnRight()
 {
+  m_direction(m_direction.y, -m_direction.x);
 }
 
 void	Snake::turnLeft()
 {
+  m_direction(-m_direction.y, m_direction.x);
 }
 
 void	Snake::update()
