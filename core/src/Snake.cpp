55df3733aafb692729ca5ed097c743bba8719efa
desabr_q <desabr_q@epitech.eu>
2014-04-01 20:19:52 +0200

    /

--- core/src/Snake.cpp
+++ core/src/Snake.cpp
@@ -5,7 +5,7 @@
 // Login   <guerot_a@epitech.net>
 //
 // Started on  Thu Mar 27 14:43:21 2014 guerot_a
-// Last update Tue Apr  1 18:59:46 2014 guerot_a
+// Last update Tue Apr  1 20:10:15 2014 Desabre Quentin
 //
 
 #include <iostream>
@@ -17,10 +17,13 @@ Snake::Snake(int width, int height) :
   m_haveTurned(false)
 {
   m_size(width, height);
-  m_snakeLimbs.push_back(Vector2i(0, 0));
-  m_snakeLimbs.push_back(Vector2i(1, 0));
-  m_snakeLimbs.push_back(Vector2i(2, 0));
+  m_snakeLimbs.push_back(Vector2i(6, 0));
+  m_snakeLimbs.push_back(Vector2i(5, 0));
+  m_snakeLimbs.push_back(Vector2i(4, 0));
   m_snakeLimbs.push_back(Vector2i(3, 0));
+  m_snakeLimbs.push_back(Vector2i(2, 0));
+  m_snakeLimbs.push_back(Vector2i(1, 0));
+  m_snakeLimbs.push_back(Vector2i(0, 0));
 }
 
 Snake::~Snake()
@@ -69,15 +72,42 @@ void	Snake::update()
     }
 }
 
-void	Snake::moveSnake()
+bool	Snake::collideMap()
 {
   if (m_snakeLimbs.front().x + m_direction.x < 0 ||
       m_snakeLimbs.front().x + m_direction.x >= m_size.x ||
       m_snakeLimbs.front().y + m_direction.y < 0 ||
       m_snakeLimbs.front().y + m_direction.y >= m_size.y)
+    return true;
+  return false;
+}
+
+bool	Snake::collideSnake()
+{
+  std::list<Vector2i>::const_iterator it;
+
+  it = m_snakeLimbs.begin();
+  it++;
+  while (it != m_snakeLimbs.end())
     {
-      m_alive = false;
+      if (((*it).x == (m_snakeLimbs.front().x + m_direction.x)) &&
+	  ((*it).y == (m_snakeLimbs.front().y + m_direction.y)))
+	return true;
+      it++;
     }
+  return false;
+
+}
+
+bool	Snake::collideObject()
+{
+
+}
+
+void	Snake::moveSnake()
+{
+  if (collideMap() || collideSnake())
+    m_alive = false;
   else
     {
       m_snakeLimbs.pop_back();
